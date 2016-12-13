e519442ea069900fdcbf9dd747c7cb0740dd38bb
lol-russo <jack.lorusso@gmail.com>
2016-12-13 22:45:44 +1100

    Completed Problem Set 0 from Lecture 1

---
+++ ps0.py
@@ -0,0 +1,13 @@
+"""
+Problem 1
+
+Write a program that does the following in order:
+Asks the user to enter his/her date of birth.
+Asks the user to enter his/her last name.
+Prints out the user’s last name and date of birth, in that order.
+
+"""
+
+dob = raw_input("Enter your date of birth.")
+lastName = raw_input("Enter your last name.")
+print(lastName + ", " + dob)
